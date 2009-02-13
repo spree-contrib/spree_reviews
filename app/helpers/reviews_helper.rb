250ec858512a3cce22b3786c9061871c22c2074d
paulcc <paulcc.two@gmail.com>
2009-02-13 12:51:10 +0000

    using _characters_ for n/5 star displays, ie not a bitmap
    this will scale better and be easier to restyle!

--- app/helpers/reviews_helper.rb
+++ app/helpers/reviews_helper.rb
@@ -1,2 +1,11 @@
 module ReviewsHelper
+
+  def star(the_class) 
+    "<span class=\"#{the_class}\"> &#10030; </span>"
+  end
+
+  def mk_stars(m)
+    (1..5).collect {|n| n <= m ? star("lit") : star("unlit") }.join
+  end
+
 end
