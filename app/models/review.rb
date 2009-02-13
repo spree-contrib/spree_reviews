d923e61e2ffa938f528ac5d628f5dc0fb6f02aec
paulcc <paulcc.two@gmail.com>
2009-02-13 15:22:24 +0000

    filtering on approved/non_approved reviews

--- app/models/review.rb
+++ app/models/review.rb
@@ -4,4 +4,7 @@ class Review < ActiveRecord::Base
   validates_presence_of :name, :location, :title, :review
   validates_numericality_of :rating, :only_integer => true
 
+  named_scope :approved,     lambda {|*args| {:conditions => "approved == 't'"}}   
+  named_scope :not_approved, lambda {|*args| {:conditions => "approved != 't'"}} 
+
 end
