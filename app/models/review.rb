16f08fbcac86413295b0dbe1a5f97a31444a24e0
Maxim <parallel588@gmail.com>
2010-12-22 00:06:57 +0500

    fix scope review

--- app/models/review.rb
+++ app/models/review.rb
@@ -4,10 +4,10 @@ class Review < ActiveRecord::Base
   validates_presence_of :title, :review
   validates_numericality_of :rating, :only_integer => true
 
-  scope :approved, :approved => '1'
-  scope :not_approved, :approved => 'f' 
+  scope :approved,  where("approved = ?", true)
+  scope :not_approved, where("approved = ?", false)
 
-  scope :approval_filter, lambda {|*args| {:conditions => ["(? = '1') || (approved = '1')", Spree::Reviews::Config[:include_unapproved_reviews]]}} 
+  scope :approval_filter, lambda {|*args| {:conditions => ["(? = ?) or (approved = ?)", Spree::Reviews::Config[:include_unapproved_reviews], true, true ]}}
 
   scope :oldest_first, :order => "created_at asc"
   scope :preview,      :limit => Spree::Reviews::Config[:preview_size], :order=>"created_at desc"
