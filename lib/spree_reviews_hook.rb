44f8e8b377489b4c9c0d727b8aeb9c73a469f705
Roman Smirnov <roman@railsdog.com>
2011-04-10 16:50:45 +0400

    Moved including assets to avoid duplication

--- lib/spree_reviews_hook.rb
+++ lib/spree_reviews_hook.rb
@@ -1,7 +1,14 @@
 class SpreeReviewsHooks < Spree::ThemeSupport::HookListener
   insert_after :product_properties, 'shared/reviews'
   insert_after :inside_head do
-    %(<%= stylesheet_link_tag('reviews.css') %>)
+    %( <%= stylesheet_link_tag('reviews.css') %> )
+  end
+  
+  insert_after :footer_right do
+    %(
+    <%= javascript_include_tag("jquery.rating.js") %>
+    <%= javascript_tag("$(document).ready(function(){$('.stars').rating({required:true});});") %>
+    )
   end
 
   insert_after :admin_product_sub_tabs do
