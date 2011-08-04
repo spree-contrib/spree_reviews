e6d34c8fae6b8b70befdd89372b531ddb785158b
Fabien Franzen <info@atelierfabien.be>
2011-08-04 16:27:11 +0200

    Added new settings: show_email and rate_helpfulness; Changed locales namespace to spree_reviews

--- app/models/reviews_configuration.rb
+++ app/models/reviews_configuration.rb
@@ -4,4 +4,10 @@ class ReviewsConfiguration < Configuration
 
   # control how many reviews are shown in summaries etc.
   preference :preview_size, :integer, :default => 3
+  
+  # show a reviewer's email address
+  preference :show_email, :boolean, :default => 'false'
+  
+  # show helpfullness rating form elements
+  preference :rate_helpfulness, :boolean, :default => 'false'  
 end
