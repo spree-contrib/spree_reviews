38cc863fcb47b84e17152235a269a89974e2902c
Josh Kalderimis <josh.kalderimis@gmail.com>
2012-02-14 23:12:01 +0100

    make the scope calls into class methods

--- app/models/spree/review.rb
+++ app/models/spree/review.rb
@@ -3,20 +3,37 @@ class Spree::Review < ActiveRecord::Base
   belongs_to :user
   has_many   :feedback_reviews
 
-  validates_presence_of :name, :review
+  validates_presence_of     :name, :review
   validates_numericality_of :rating, :only_integer => true
+
   default_scope order("spree_reviews.created_at DESC")
-  scope :approved,  where("approved = ?", true)
-  scope :not_approved, where("approved = ?", false)
 
-  scope :approval_filter, lambda {|*args| {:conditions => ["(? = ?) or (approved = ?)", Spree::Reviews::Config[:include_unapproved_reviews], true, true ]}}
-
-  scope :oldest_first, :order => "created_at asc"
-  scope :preview,      :limit => Spree::Reviews::Config[:preview_size], :order=>"created_at desc"
   attr_protected :user_id, :product_id, :ip_address
+
+  class << self
+    def approved
+      where("approved = ?", true)
+    end
+
+    def not_approved
+      where("approved = ?", false)
+    end
+
+    def approval_filter
+      where(["(? = ?) or (approved = ?)", Spree::Reviews::Config[:include_unapproved_reviews], true, true])
+    end
+
+    def oldest_first
+      order("created_at asc")
+    end
+
+    def preview
+      limit(Spree::Reviews::Config[:preview_size]).oldest_first
+    end
+  end
 
   def feedback_stars
     return 0 if feedback_reviews.count <= 0
-    ((feedback_reviews.sum(:rating)/feedback_reviews.count) + 0.5).floor
+    ((feedback_reviews.sum(:rating) / feedback_reviews.count) + 0.5).floor
   end
 end
