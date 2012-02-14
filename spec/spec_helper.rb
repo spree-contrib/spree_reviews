6913de8249e21593426914b81fb78dd35960da16
Josh Kalderimis <josh.kalderimis@gmail.com>
2012-02-14 23:12:24 +0100

    SPACES PEOPLE!

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,7 +5,7 @@ require 'rspec/rails'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
-Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
+Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 
 # Requires factories defined in spree_core
 require 'spree/core/testing_support/factories'
