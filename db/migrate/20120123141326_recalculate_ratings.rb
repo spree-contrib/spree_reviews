780f320022353cdc7d7f7f81035499d7e5ff0e4f
Ryan Bigg <radarlistener@gmail.com>
2012-02-14 19:20:31 -0500

    Recalculate ratings migration should only update products with their *approved* reviews count

--- db/migrate/20120123141326_recalculate_ratings.rb
+++ db/migrate/20120123141326_recalculate_ratings.rb
@@ -2,7 +2,7 @@ class RecalculateRatings < ActiveRecord::Migration
   def up
     Spree::Product.reset_column_information
     Spree::Product.all.each do |p|
-      Spree::Product.update_counters p.id, :reviews_count => p.reviews.length
+      Spree::Product.update_counters p.id, :reviews_count => p.reviews.approved.length
       p.recalculate_rating
     end
   end
