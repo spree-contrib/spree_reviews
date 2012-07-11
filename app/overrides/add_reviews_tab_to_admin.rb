688fed84ffd14b6111749137bfdd487db02d1bf9
Johan Bruning <johan.h.bruning@gmail.com>
2012-07-11 08:23:30 +0200

    Override different template in order to have reviews as a submenu item from products

--- app/overrides/add_reviews_tab_to_admin.rb
+++ app/overrides/add_reviews_tab_to_admin.rb
@@ -1,5 +1,4 @@
-Deface::Override.new(:virtual_path => "spree/layouts/admin",
+Deface::Override.new(:virtual_path => "spree/admin/shared/_product_sub_menu",
                      :name => "reviews_admin_tab",
-                     :insert_bottom => "[data-hook='admin_tabs']",
-                     :text => "<%= tab(:reviews, :label => 'review_management') %>",
-                     :disabled => false)
+                     :insert_bottom => "[data-hook='admin_product_sub_tabs']",
+                     :text => "<%= tab(:reviews, :label => 'review_management') %>")
