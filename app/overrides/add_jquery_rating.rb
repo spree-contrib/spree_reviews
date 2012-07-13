5f5c67a2589f1108944d7ef98d8c81416504ec40
Johan Bruning <johan@wirelab.nl>
2012-07-13 12:25:15 +0200

    Removed unused override
    Merges #19

--- app/overrides/add_jquery_rating.rb
+++
@@ -1,8 +0,0 @@
-Deface::Override.new(:virtual_path => "spree/shared/_footer",
-                     :name => "converted_footer_right_920701005",
-                     :insert_after => "[data-hook='footer_right'], #footer_right[data-hook]",
-                     :text => "
-    <%= javascript_include_tag(\"jquery.rating.js\") %>
-    <%= javascript_tag(\"$(document).ready(function(){$('.stars').rating({required:true});});\") %>
-    ",
-                     :disabled => false)
