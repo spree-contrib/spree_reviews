913ffd50020d63df327e43b98a9fc9c5aa6b898f
Denis Ivanov <visible.h4x@gmail.com>
2012-08-01 16:33:02 -0400

    Refactoring a large rating recalculation migration for performance

--- db/migrate/20120123141326_recalculate_ratings.rb
+++ db/migrate/20120123141326_recalculate_ratings.rb
@@ -1,13 +1,17 @@
 class RecalculateRatings < ActiveRecord::Migration
   def up
     Spree::Product.reset_column_information
-    Spree::Product.all.each do |p|
+
+    Spree::Product.update_all :reviews_count => 0
+    
+    Spree::Product.joins(:reviews).where("spree_reviews.id IS NOT NULL").find_each do |p|
       Spree::Product.update_counters p.id, :reviews_count => p.reviews.approved.length
-      
+
       # recalculate_product_rating exists on the review, not the product
       if p.reviews.approved.count > 0
         p.reviews.approved.first.recalculate_product_rating
       end
+
     end
   end
 
