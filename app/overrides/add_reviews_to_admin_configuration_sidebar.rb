e054547d01832e7b4c9da740b9b0aaf0033efad6
Víctor Martín <ilovemayalopez@gmail.com>
2012-09-24 18:27:00 +0200

    Reviews rating link in the admin configuration sidebar

---
+++ app/overrides/add_reviews_to_admin_configuration_sidebar.rb
@@ -0,0 +1,5 @@
+Deface::Override.new(:virtual_path => "spree/admin/shared/_configuration_menu",
+                     :name => "converted_admin_configurations_menu",
+                     :insert_bottom => "[data-hook='admin_configurations_sidebar_menu'], #admin_configurations_sidebar_menu[data-hook]",
+                     :text => "<%= configurations_sidebar_menu_item t('spree_reviews.review_settings'), admin_review_settings_path %>",
+                     :disabled => false)
