2a41d8f0831aa021a21519b84638f7afd3a3a476
Jeff Dutil <jdutil21@gmail.com>
2012-10-03 17:40:01 -0400

    Fix override.

--- app/overrides/fix_highlighting_of_products_tab.rb
+++ app/overrides/fix_highlighting_of_products_tab.rb
@@ -1,5 +1,5 @@
 Deface::Override.new(:virtual_path => "spree/admin/shared/_tabs",
                     :name => "reviews_admin_tab_root",
-                    :insert_bottom => "code[erb-loud]:contains('tab :products')",
-                    :text => ", :reviews",
-                    :original => '84f31f710e4f2e7a5d88037387a7cffbac45d280')
+                    :replace_contents => "code[erb-loud]:contains('tab :products')",
+                    :text => "tab :products, :option_types, :properties, :prototypes, :variants, :product_properties, :taxons, :reviews, :icon => 'icon-th-large'",
+                    :original => '0abc74602592114216d5a53bc095823ec6759895')
