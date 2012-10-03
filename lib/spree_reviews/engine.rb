2614722423438f664dc2b397971db62b7391930b
Jeff Dutil <jdutil21@gmail.com>
2012-10-02 20:38:38 -0400

    Update Engine railtie.

--- lib/spree_reviews/engine.rb
+++ lib/spree_reviews/engine.rb
@@ -1,16 +1,21 @@
 module SpreeReviews
   class Engine < Rails::Engine
+    isolate_namespace Spree
     engine_name 'spree_reviews'
 
     config.autoload_paths += %W(#{config.root}/lib)
 
-    def self.activate
-      Dir.glob(File.join(File.dirname(__FILE__), "../../app/**/*_decorator*.rb")) do |c|
-        Rails.env.production? ? require(c) : load(c)
-      end
-      Spree::Ability.register_ability(Spree::ReviewsAbility)
+    initializer "spree_reviews.environment", :before => :load_config_initializers do |app|
+      Spree::Reviews::Config = Spree::ReviewsConfiguration.new
     end
 
-    config.to_prepare &method(:activate).to_proc
+    def self.activate
+       Dir.glob(File.join(File.dirname(__FILE__), "../../../app/**/*_decorator*.rb")) do |c|
+         Rails.configuration.cache_classes ? require(c) : load(c)
+       end
+       Spree::Ability.register_ability(Spree::ReviewsAbility)
+     end
+     config.to_prepare &method(:activate).to_proc
+
   end
 end
