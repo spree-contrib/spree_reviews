e87b09da31a22f40243fe44a5d41296c709400c3
Jeff Dutil <jdutil21@gmail.com>
2012-10-02 21:48:12 -0400

    Fix railtie order.

--- lib/spree_reviews/engine.rb
+++ lib/spree_reviews/engine.rb
@@ -1,21 +1,22 @@
 module SpreeReviews
   class Engine < Rails::Engine
+    require 'spree/core'
     isolate_namespace Spree
     engine_name 'spree_reviews'
 
     config.autoload_paths += %W(#{config.root}/lib)
 
-    initializer "spree_reviews.environment", :before => :load_config_initializers do |app|
-      Spree::Reviews::Config = Spree::ReviewsConfiguration.new
-    end
-
     def self.activate
-       Dir.glob(File.join(File.dirname(__FILE__), "../../../app/**/*_decorator*.rb")) do |c|
-         Rails.configuration.cache_classes ? require(c) : load(c)
-       end
-       Spree::Ability.register_ability(Spree::ReviewsAbility)
-     end
-     config.to_prepare &method(:activate).to_proc
+      Dir.glob(File.join(File.dirname(__FILE__), "../../../app/**/*_decorator*.rb")) do |c|
+        Rails.configuration.cache_classes ? require(c) : load(c)
+      end
+      Spree::Ability.register_ability(Spree::ReviewsAbility)
+    end
+    config.to_prepare &method(:activate).to_proc
+
+    initializer "spree_reviews.environment", :before => :load_config_initializers do |app|
+      SpreeReviews::Config = Spree::ReviewsConfiguration.new
+    end
 
   end
 end
