d4611ac373490a8ab362f11c29997ff9db9510f3
Jeff Dutil <jdutil21@gmail.com>
2012-10-02 23:38:04 -0400

    Properly load decorators.

--- lib/spree_reviews/engine.rb
+++ lib/spree_reviews/engine.rb
@@ -7,12 +7,17 @@ module SpreeReviews
     config.autoload_paths += %W(#{config.root}/lib)
 
     def self.activate
-      Dir.glob(File.join(File.dirname(__FILE__), "../../../app/**/*_decorator*.rb")) do |c|
+      Dir.glob(File.join(File.dirname(__FILE__), '../../app/**/*_decorator*.rb')) do |c|
         Rails.configuration.cache_classes ? require(c) : load(c)
       end
       Spree::Ability.register_ability(Spree::ReviewsAbility)
     end
     config.to_prepare &method(:activate).to_proc
+
+    # use rspec for tests
+    config.generators do |g|
+      g.test_framework :rspec
+    end
 
   end
 end
