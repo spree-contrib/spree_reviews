1d166ae3fc4744edda1f4073ef2b3cccf29524b7
Michael Bianco <iloveitaly@gmail.com>
2012-10-09 16:44:24 -0400

    Gemfile + gemspec cleanup, tests are passing. Removed old Rake + spec opts file as well

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,7 @@
 ENV["RAILS_ENV"] = "test"
 require File.expand_path("../dummy/config/environment.rb",  __FILE__)
 require 'rspec/rails'
-require 'faker'
+require 'ffaker'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
