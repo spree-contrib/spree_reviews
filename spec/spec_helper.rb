f3a931182c6674b77e85d50642ecd80d71649139
Michael Bianco <iloveitaly@gmail.com>
2013-02-05 13:59:40 -0500

    Updating testing configuration

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,16 +4,22 @@ require File.expand_path("../dummy/config/environment.rb",  __FILE__)
 require 'rspec/rails'
 require 'ffaker'
 
+require 'capybara'
+require 'capybara/rspec'
+require 'capybara/rails'
+
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
 Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 
-# Requires factories defined in spree_core
+# Requires factosb ries defined in spree_core
 require 'spree/core/testing_support/factories'
+require 'spree/core/testing_support/controller_requests'
+require 'spree/core/testing_support/authorization_helpers'
+require 'spree/core/url_helpers'
 
 RSpec.configure do |config|
-  # If you're not using ActiveRecord, or you'd prefer not to run each of your
-  # examples within a transaction, remove the following line or assign false
-  # instead of true.
+  config.include FactoryGirl::Syntax::Methods
+  config.include Spree::Core::UrlHelpers
   config.use_transactional_fixtures = true
 end
