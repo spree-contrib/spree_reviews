dc3336e19a35800377567202091a6eba5120d759
Denis Ivanov <visible.h4x@gmail.com>
2013-03-08 10:56:01 -0800

    Fixing Authorization error w/ CanCan

--- app/models/spree/reviews_ability.rb
+++ app/models/spree/reviews_ability.rb
@@ -1,12 +1,12 @@
 class Spree::ReviewsAbility
   include CanCan::Ability
 
-  def initialize(user)
+  def initialize user
     can :create, Spree::Review do |review|
-      !Spree::Reviews::Config[:require_login]
+      !Spree::Reviews::Config[:require_login] || (user.email and !user.anonymous?)
     end
     can :create, Spree::FeedbackReview do |review|
-      !Spree::Reviews::Config[:require_login]
+      !Spree::Reviews::Config[:require_login] || (user.email and !user.anonymous?)
     end
   end
 end
