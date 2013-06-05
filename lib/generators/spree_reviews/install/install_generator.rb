12864c3e98bfc13ff8b56fcf0ea2e484a24ffeb7
Jeff Dutil <jdutil@burlingtonwebapps.com>
2013-06-04 23:33:55 -0400

    Fixes for Rakefile and Spree 2.

--- lib/generators/spree_reviews/install/install_generator.rb
+++ lib/generators/spree_reviews/install/install_generator.rb
@@ -2,6 +2,8 @@ module SpreeReviews
   module Generators
     class InstallGenerator < Rails::Generators::Base
 
+      class_option :auto_run_migrations, :type => :boolean, :default => false
+
       def add_javascripts
         append_file "app/assets/javascripts/store/all.js", "//= require jquery.rating\n"
       end
@@ -15,12 +17,12 @@ module SpreeReviews
       end
 
       def run_migrations
-         res = ask "Would you like to run the migrations now? [Y/n]"
-         if res == "" || res.downcase == "y"
-           run 'rake db:migrate'
-         else
-           puts "Skiping rake db:migrate, don't forget to run it!"
-         end
+        run_migrations = options[:auto_run_migrations] || ['', 'y', 'Y'].include?(ask 'Would you like to run the migrations now? [Y/n]')
+        if run_migrations
+          run 'bundle exec rake db:migrate'
+        else
+          puts 'Skipping rake db:migrate, don\'t forget to run it!'
+        end
       end
     end
   end
