789d7a1e2cd62011c72de363455a4dead1b7b2b9
Jeff Dutil <jdutil@burlingtonwebapps.com>
2013-06-04 23:33:55 -0400

    Fixes for Rakefile and Spree 2.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,17 +10,17 @@ require 'capybara/rails'
 
 Dir[File.join(File.dirname(__FILE__), "support/**/*.rb")].each {|f| require f }
 
-require 'spree/core/testing_support/factories'
-require 'spree/core/testing_support/controller_requests'
-require 'spree/core/testing_support/authorization_helpers'
-require 'spree/core/url_helpers'
+require 'spree/testing_support/factories'
+require 'spree/testing_support/controller_requests'
+require 'spree/testing_support/authorization_helpers'
+require 'spree/testing_support/url_helpers'
 
 Dir[File.join(File.dirname(__FILE__), "factories/*.rb")].each {|f| require f }
 
 RSpec.configure do |config|
-  config.include Spree::Core::TestingSupport::ControllerRequests
+  config.include Spree::TestingSupport::ControllerRequests
   config.include FactoryGirl::Syntax::Methods
-  config.include Spree::Core::UrlHelpers
+  config.include Spree::TestingSupport::UrlHelpers
   config.extend AuthorizationHelpers::Request, :type => :feature
   config.use_transactional_fixtures = false
 end
