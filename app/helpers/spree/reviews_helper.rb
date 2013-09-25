e6225a640b69c2316645fc7351575db268c40de3
Uri Mikhli <urimikhli@gmail.com>
2013-09-25 10:34:39 -0400

    null ratings happen, even though they shouldnt. Set the default rating to be 5       so that txt_stars() doesnt hurk on the null

--- app/helpers/spree/reviews_helper.rb
+++ app/helpers/spree/reviews_helper.rb
@@ -8,7 +8,7 @@ module Spree::ReviewsHelper
     (1..5).collect {|n| n <= m ? star("lit") : star("unlit") }.join
   end
 
-  def txt_stars(n, show_out_of = true)
+  def txt_stars(n=5, show_out_of = true)
     res = I18n.t('star', :count => n)
     res += " #{t('out_of_5')}" if show_out_of
     res
