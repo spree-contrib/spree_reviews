b648e93baf442443f024062d0d7e51d2ecedbacf
Uri Mikhli <urimikhli@gmail.com>
2013-09-25 11:41:22 -0400

    In txt_stars the input variable has been renamed to rating for clarity.  and the default value to prevent nulls has been refactored.

--- app/helpers/spree/reviews_helper.rb
+++ app/helpers/spree/reviews_helper.rb
@@ -8,8 +8,9 @@ module Spree::ReviewsHelper
     (1..5).collect {|n| n <= m ? star("lit") : star("unlit") }.join
   end
 
-  def txt_stars(n=5, show_out_of = true)
-    res = I18n.t('star', :count => n)
+  def txt_stars(rating, show_out_of = true)
+    rating ||= 5
+    res = I18n.t('star', :count => rating)
     res += " #{t('out_of_5')}" if show_out_of
     res
   end
