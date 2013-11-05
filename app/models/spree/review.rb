c304ab77bc9e343ae55a2d16e0414fbff7f9f3e2
Jeff Dutil <jdutil@burlingtonwebapps.com>
2013-11-04 23:36:09 -0500

    Dont delete product when review is deleted... Fixes #79
    Conflicts:
    	app/models/spree/review.rb

--- app/models/spree/review.rb
+++ app/models/spree/review.rb
@@ -1,5 +1,6 @@
 class Spree::Review < ActiveRecord::Base
-  belongs_to :product
+
+  belongs_to :product, touch: true
   belongs_to :user, :class_name => Spree.user_class.to_s
   has_many   :feedback_reviews
 
