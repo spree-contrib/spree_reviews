9872fab007d59a31af50196c0bad1ce8ba718f5c
Walter Mottinelli <walter.mottinelli@triboo.it>
2013-11-22 17:00:32 +0100

    Fix for deface 1.0 in fix_highlighting_of_products_tab template
    Fixes #82

--- app/overrides/fix_highlighting_of_products_tab.rb
+++ app/overrides/fix_highlighting_of_products_tab.rb
@@ -1,5 +1,5 @@
 Deface::Override.new(:virtual_path => "spree/admin/shared/_tabs",
                     :name => "reviews_admin_tab_root",
-                    :replace_contents => "code[erb-loud]:contains('tab :products')",
+                    :replace_contents => "erb[loud]:contains('tab :products')",
                     :text => "tab :products, :option_types, :properties, :prototypes, :variants, :product_properties, :taxons, :reviews, :icon => 'icon-th-large'",
                     :original => '0abc74602592114216d5a53bc095823ec6759895')
