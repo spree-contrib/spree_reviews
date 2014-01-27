d06cdda30d3680129817388f0dc0884475c82160
Daniel J. Pritchett <dpritchett@gmail.com>
2014-01-27 15:01:50 -0600

    Removed calls to deprecated User#anonymous?

--- app/models/spree/reviews_ability.rb
+++ app/models/spree/reviews_ability.rb
@@ -4,10 +4,10 @@ class Spree::ReviewsAbility
   def initialize user
     review_ability_class = self.class
     can :create, Spree::Review do |review|
-      review_ability_class.allow_anonymous_reviews? || (user.email and !user.anonymous?)
+      review_ability_class.allow_anonymous_reviews? || !user.email.blank?
     end
     can :create, Spree::FeedbackReview do |review|
-      review_ability_class.allow_anonymous_reviews? || (user.email and !user.anonymous?)
+      review_ability_class.allow_anonymous_reviews? || !user.email.blank?
     end
   end
 
