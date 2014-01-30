314644cf11aa396bbe105bd39afc42b817aa5d6e
Ryan Bigg <radarlistener@gmail.com>
2014-01-30 13:07:32 +1100

    Require spree assets so that Spree variable is available in admin tests

--- lib/generators/spree_reviews/install/install_generator.rb
+++ lib/generators/spree_reviews/install/install_generator.rb
@@ -6,10 +6,14 @@ module SpreeReviews
 
       def add_javascripts
         append_file "app/assets/javascripts/store/all.js", "//= require jquery.rating\n"
+        append_file "app/assets/javascripts/store/all.js", "//= require store/spree_frontend\n"
+
+        append_file "app/assets/javascripts/admin/all.js", "//= require admin/spree_backend\n"
       end
 
       def add_stylesheets
         inject_into_file "app/assets/stylesheets/store/all.css", " *= require store/spree_reviews\n", :before => /\*\//, :verbose => true
+        inject_into_file "app/assets/stylesheets/store/all.css", " *= require store/spree_frontend\n", :before => /\*\//, :verbose => true
       end
 
       def add_migrations
