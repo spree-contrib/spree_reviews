16327ade1f1af6753ac9a0ee58178186d1ba5a0d
Tobias Bohwalli <hi@futhr.io>
2014-01-31 07:00:35 +0100

    More specs now 100% coverage
    Made settings spec less cryptic.

--- spec/controllers/feedback_reviews_controller_spec.rb
+++ spec/controllers/feedback_reviews_controller_spec.rb
@@ -1,37 +1,58 @@
 require 'spec_helper'
 
 describe Spree::FeedbackReviewsController do
-  let(:token) { 'some_token' }
   let(:user) { create(:user) }
   let(:product) { create(:product) }
-  let(:review) { create(:review, :approved, product: product) }
-
-  before do
-    controller.stub :spree_current_user => user
-    controller.stub :spree_user_signed_in? => true
+  let(:review) { create(:review, user: user) }
+  let(:valid_attributes) do
+    { review_id: review.id,
+      user_id: user.id,
+      feedback_review: {
+        rating: '4 stars',
+        comment: 'some comment'
+      }}
   end
 
-  context '#create' do
-    context 'for a valid review' do
-      context 'with valid params' do
-        it 'creates a new feedback review' do
-          rating = 4
-          comment = Faker::Lorem.paragraphs(3).join("\n")
-          expect {
-            spree_post :create, { review_id: review.id,
-                                  feedback_review: { comment: comment,
-                                                     rating: rating },
-                                  format: :js },
-                                { access_token: token }
-            response.status.should eq(200)
-            response.should render_template(:create)
-          }.to change(Spree::Review, :count).by(1)
-          feedback_review = Spree::FeedbackReview.last
-          feedback_review.comment.should eq(comment)
-          feedback_review.review.should eq(review)
-          feedback_review.rating.should eq(rating)
-        end
-      end
+  before do
+    controller.stub spree_current_user: user
+    controller.stub spree_user_signed_in?: true
+    request.env['HTTP_REFERER'] = '/'
+  end
+
+  describe '#create' do
+    it 'creates a feedback-review' do
+      expect {
+        spree_post :create, valid_attributes
+      }.to change(Spree::FeedbackReview, :count).from(0).to(1)
+    end
+
+    it 'redirects back to the calling page' do
+      spree_post :create, valid_attributes
+      response.should redirect_to '/'
+    end
+
+    it 'sets locale on feedback-review if required by config' do
+      Spree::Reviews::Config.preferred_track_locale = true
+      spree_post :create, valid_attributes
+      assigns[:review].locale.should eq I18n.locale.to_s
+    end
+
+    it 'fails when user is not authorized' do
+      controller.stub(:authorize!) { raise }
+      expect {
+        spree_post :create, valid_attributes
+      }.to raise_error
+    end
+
+    it 'removes all non-numbers from ratings parameter' do
+      spree_post :create, valid_attributes
+      controller.params[:feedback_review][:rating].should eq '4'
+    end
+
+    it 'do not create feedback-review if review doesnt exist' do
+      expect {
+        spree_post :create, valid_attributes.merge!({review_id: nil})
+      }.to raise_error
     end
   end
 end
