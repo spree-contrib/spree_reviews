d113249d7b3ece42040851d78fac265d3e61f72b
Tobias Bohwalli <hi@futhr.io>
2014-01-31 07:00:35 +0100

    More specs now 100% coverage
    Made settings spec less cryptic.

--- spec/controllers/admin/review_settings_controller_spec.rb
+++ spec/controllers/admin/review_settings_controller_spec.rb
@@ -9,22 +9,42 @@ describe Spree::Admin::ReviewSettingsController do
   end
 
   context '#update' do
-    it 'should update the settings when passed correct parameters' do
-      preferences_params = {}
-      existing_settings = Spree::ReviewsConfiguration.new
-      Spree::ReviewsConfiguration.boolean_preferences.each do |p|
-        preferences_params[p.to_sym] = !existing_settings.get_preference(p)
-      end
-      new_preview_size = 1 + SecureRandom.random_number(10)
-      preferences_params[:preview_size] = new_preview_size
-      spree_post :update, { preferences: preferences_params }
-      response.should redirect_to(spree.edit_admin_review_settings_path)
+    it 'redirects to edit-review-settings page' do
+      spree_put :update, preferences: { preview_size: 4 }
+      response.should redirect_to spree.edit_admin_review_settings_path
+    end
 
-      new_settings = Spree::ReviewsConfiguration.new
-      Spree::ReviewsConfiguration.boolean_preferences.each do |p|
-        new_settings.get_preference(p).should eq(preferences_params[p.to_sym])
+    context 'For parameters:
+            preview_size: 4,
+            show_email: false,
+            feedback_rating: false,
+            require_login: true,
+            track_locale: true' do
+
+      it 'sets preferred_preview_size to 4' do
+        spree_put :update, preferences: { preview_size: 4 }
+        Spree::Reviews::Config.preferred_preview_size.should eq 4
       end
-      new_settings.get_preference(:preview_size).should eq(new_preview_size)
+
+      it 'sets preferred_show_email to false' do
+        spree_put :update, preferences: { show_email: false }
+        Spree::Reviews::Config.preferred_show_email.should be_false
+      end
+
+      it 'sets preferred_feedback_rating to false' do
+        spree_put :update, preferences: { feedback_rating: false }
+        Spree::Reviews::Config.preferred_feedback_rating.should be_false
+      end
+
+      it 'sets preferred_require_login to true' do
+        spree_put :update, preferences: { require_login: true }
+        Spree::Reviews::Config.preferred_require_login.should be_true
+      end
+
+      it 'sets preferred_track_locale to true' do
+        spree_put :update, preferences: { track_locale: true }
+        Spree::Reviews::Config.preferred_track_locale.should be_true
+      end
     end
   end
 
--- spec/controllers/admin/reviews_controller_spec.rb
+++ spec/controllers/admin/reviews_controller_spec.rb
@@ -4,6 +4,7 @@ describe Spree::Admin::ReviewsController do
   stub_authorization!
 
   let(:product) { create(:product) }
+  let(:review) { create(:review, approved: false) }
 
   before do
     user = create(:admin_user)
@@ -18,6 +19,45 @@ describe Spree::Admin::ReviewsController do
       ]
       spree_get :index, product_id: product.permalink
       assigns[:reviews].should =~ reviews
+    end
+  end
+
+  context '#approve' do
+    it 'show notice message when approved' do
+      review.update_attribute(:approved, true)
+      spree_get :approve, id: review.id
+      response.should redirect_to spree.admin_reviews_path
+      flash[:notice].should eq Spree.t(:info_approve_review)
+    end
+
+    it 'show error message when not approved' do
+      Spree::Review.any_instance.stub(:update_attribute).and_return(false)
+      spree_get :approve, id: review.id
+      flash[:error].should eq Spree.t(:error_approve_review)
+    end
+  end
+
+  context '#edit' do
+    specify do
+      spree_get :edit, id: review.id
+      response.status.should eq(200)
+    end
+
+    context 'when product is nil' do
+      before do
+        review.product = nil
+        review.save!
+      end
+
+      it 'flash error' do
+        spree_get :edit, id: review.id
+        flash[:error].should eq Spree.t(:error_no_product)
+      end
+
+      it 'redirect to admin-reviews page' do
+        spree_get :edit, id: review.id
+        response.should redirect_to spree.admin_reviews_path
+      end
     end
   end
 end
