314ccc4d40cdf5b6d0f10ce4c1de9059c8d70419
Tim <thogg@deseretbook.com>
2014-07-30 11:09:16 -0600

    add index for show_identifier

--- db/migrate/20140703200946_add_show_identifier_to_reviews.rb
+++ db/migrate/20140703200946_add_show_identifier_to_reviews.rb
@@ -1,5 +1,6 @@
 class AddShowIdentifierToReviews < ActiveRecord::Migration
   def change
     add_column :spree_reviews, :show_identifier, :boolean, default: true
+    add_index :spree_reviews, :show_identifier
   end
 end
