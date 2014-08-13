33b5217835d6d454ff191a6a18b3f48b1fb5976e
Brian Buchalter <bal711@gmail.com>
2014-08-13 10:10:54 -0400

    Remove unwanted deface for performance gain

--- app/overrides/add_reviews_after_product_properties.rb
+++
@@ -1,5 +0,0 @@
-Deface::Override.new(:virtual_path => "spree/products/show",
-                     :name => "converted_product_properties_767643482",
-                     :insert_after => "[data-hook='product_properties']",
-                     :partial => "spree/shared/reviews",
-                     :disabled => false)
