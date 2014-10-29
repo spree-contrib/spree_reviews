16e6a4ccff6b0718e631e777511aa8d7b16144dd
Brian Buchalter <bal711@gmail.com>
2014-10-29 12:21:42 -0600

    Remove unnessisary deface override fixes #120

--- app/overrides/fix_highlighting_of_products_tab.rb
+++
@@ -1,5 +0,0 @@
-Deface::Override.new(:virtual_path => "spree/admin/shared/_tabs",
-                    :name => "reviews_admin_tab_root",
-                    :replace_contents => "erb[loud]:contains('tab :products')",
-                    :text => "tab :products, :option_types, :properties, :prototypes, :variants, :product_properties, :taxons, :reviews, :icon => 'icon-th-large'",
-                    :original => '0abc74602592114216d5a53bc095823ec6759895')
