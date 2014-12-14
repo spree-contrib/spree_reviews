b9d16147edc75ce25cd83dd8ac21008621fcc75c
Tobias Bohwalli <hi@futhr.io>
2014-12-14 01:17:33 +0100

    Update layout for master branch.
    Still some tweaks can be made to the public stars layout.

--- app/overrides/add_reviews_tab_to_admin.rb
+++ app/overrides/add_reviews_tab_to_admin.rb
@@ -1,4 +1,4 @@
-Deface::Override.new(virtual_path: 'spree/admin/shared/_product_sub_menu',
+Deface::Override.new(virtual_path: 'spree/admin/shared/sub_menu/_product',
                      name: 'reviews_admin_tab',
                      insert_bottom: '[data-hook="admin_product_sub_tabs"]',
                      text: '<%= tab(:reviews, label: "review_management") %>',
