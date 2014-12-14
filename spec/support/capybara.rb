6b9651d88b7b2a27d80d1dcc25208be702b71711
Tobias Bohwalli <hi@futhr.io>
2014-12-14 01:29:13 +0100

    Fullscreen mode when run Capybara with selenium.

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -3,6 +3,12 @@ require 'capybara/rspec'
 require 'capybara/rails'
 require 'capybara/poltergeist'
 
-RSpec.configure do
+RSpec.configure do |config|
   Capybara.javascript_driver = :poltergeist
+
+  config.before(:each, :js) do
+    if Capybara.javascript_driver == :selenium
+      page.driver.browser.manage.window.maximize
+    end
+  end
 end
