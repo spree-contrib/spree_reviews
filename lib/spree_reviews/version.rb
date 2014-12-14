63228d3c783fe26de28c0fe2691ca0cc0c6f680a
Tobias Bohwalli <hi@futhr.io>
2014-12-14 18:09:51 +0100

    Add CHANGELOG.md and bump version.

--- lib/spree_reviews/version.rb
+++ lib/spree_reviews/version.rb
@@ -8,10 +8,10 @@ module SpreeReviews
   end
 
   module VERSION
-    MAJOR = 1
+    MAJOR = 3
     MINOR = 0
     TINY  = 0
-    PRE   = nil
+    PRE   = 'beta'
 
     STRING = [MAJOR, MINOR, TINY, PRE].compact.join('.')
   end
