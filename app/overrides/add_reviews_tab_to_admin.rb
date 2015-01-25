90ae0f9a626df1ada66b31bf2fc57a71e1546c8f
Tobias Bohwalli <hi@futhr.io>
2015-01-25 20:44:42 +0100

    Remove Deface option "disabled".
    Disabled option is unnecessary and may just raise deprecation warnings.

--- app/overrides/add_reviews_tab_to_admin.rb
+++ app/overrides/add_reviews_tab_to_admin.rb
@@ -1,5 +1,6 @@
-Deface::Override.new(virtual_path: 'spree/admin/shared/sub_menu/_product',
-                     name: 'reviews_admin_tab',
-                     insert_bottom: '[data-hook="admin_product_sub_tabs"]',
-                     text: '<%= tab(:reviews, label: "review_management") %>',
-                     disabled: false)
+Deface::Override.new(
+  virtual_path: 'spree/admin/shared/sub_menu/_product',
+  name: 'reviews_admin_tab',
+  insert_bottom: '[data-hook="admin_product_sub_tabs"]',
+  text: '<%= tab(:reviews, label: "review_management") %>'
+)
--- app/overrides/add_reviews_to_admin_configuration_sidebar.rb
+++ app/overrides/add_reviews_to_admin_configuration_sidebar.rb
@@ -1,5 +1,6 @@
-Deface::Override.new(virtual_path: 'spree/admin/shared/sub_menu/_configuration',
-                     name: 'converted_admin_configurations_menu',
-                     insert_bottom: '[data-hook="admin_configurations_sidebar_menu"]',
-                     text: '<%= configurations_sidebar_menu_item Spree.t(:review_settings, scope: :spree_reviews), edit_admin_review_settings_path %>',
-                     disabled: false)
+Deface::Override.new(
+  virtual_path: 'spree/admin/shared/sub_menu/_configuration',
+  name: 'converted_admin_configurations_menu',
+  insert_bottom: '[data-hook="admin_configurations_sidebar_menu"]',
+  text: '<%= configurations_sidebar_menu_item Spree.t(:review_settings, scope: :spree_reviews), edit_admin_review_settings_path %>'
+)
