fb236953dfa7c8bd77dd6d2472312911ee8bfb94
Tobias Bohwalli <hi@futhr.io>
2015-02-12 07:05:25 +0100

    Cosmetic change use subject in spec.

--- spec/controllers/admin/review_settings_controller_spec.rb
+++ spec/controllers/admin/review_settings_controller_spec.rb
@@ -22,39 +22,41 @@ RSpec.describe Spree::Admin::ReviewSettingsController, type: :controller do
             track_locale: true
             show_identifier: true' do
 
+      subject { Spree::Reviews::Config }
+
       it 'sets preferred_include_unapproved_reviews to false' do
         spree_put :update, preferences: { include_unapproved_reviews: true }
-        expect(Spree::Reviews::Config.preferred_include_unapproved_reviews).to be(true)
+        expect(subject.preferred_include_unapproved_reviews).to be(true)
       end
 
       it 'sets preferred_preview_size to 4' do
         spree_put :update, preferences: { preview_size: 4 }
-        expect(Spree::Reviews::Config.preferred_preview_size).to be(4)
+        expect(subject.preferred_preview_size).to be(4)
       end
 
       it 'sets preferred_show_email to false' do
         spree_put :update, preferences: { show_email: true }
-        expect(Spree::Reviews::Config.preferred_show_email).to be(true)
+        expect(subject.preferred_show_email).to be(true)
       end
 
       it 'sets preferred_feedback_rating to false' do
         spree_put :update, preferences: { feedback_rating: true }
-        expect(Spree::Reviews::Config.preferred_feedback_rating).to be(true)
+        expect(subject.preferred_feedback_rating).to be(true)
       end
 
       it 'sets preferred_require_login to true' do
         spree_put :update, preferences: { require_login: false }
-        expect(Spree::Reviews::Config.preferred_require_login).to be(false)
+        expect(subject.preferred_require_login).to be(false)
       end
 
       it 'sets preferred_track_locale to true' do
         spree_put :update, preferences: { track_locale: true }
-        expect(Spree::Reviews::Config.preferred_track_locale).to be(true)
+        expect(subject.preferred_track_locale).to be(true)
       end
 
       it 'sets preferred_show_identifier to false' do
         spree_put :update, preferences: { show_identifier: true }
-        expect(Spree::Reviews::Config.preferred_show_identifier).to be(true)
+        expect(subject.preferred_show_identifier).to be(true)
       end
     end
   end
