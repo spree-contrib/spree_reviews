c231bb307a2a772d560e0f52fd1309e1acd98824
Lydia Khashina <adatheleader@gmail.com>
2015-05-03 01:32:47 +0300

    avg_rating and reviews_count fields are available on Spree API side

--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -1,7 +1,13 @@
 RSpec.describe Spree::ProductsController, type: :controller do
-  [:avg_rating, :reviews_count].each do |attrib|
+  reviews_fields = [:avg_rating, :reviews_count]
+  reviews_fields.each do |attrib|
     it "adds #{attrib} to the set of allowed attributes" do
       expect(controller.permitted_product_attributes).to include(attrib)
+    end
+  end
+  reviews_fields.each do |attrib|
+    it "adds #{attrib} to the set of available attributes from Spree API" do
+      expect(Spree::Api::ApiHelpers.product_attributes).to include(attrib)
     end
   end
 end
