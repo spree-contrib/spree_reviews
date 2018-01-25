4bf6542dbd80814601de6bbc26189ee2eb42dd85
Artem Kozaev <artemkozaev@gmail.com>
2018-01-25 16:56:01 +0300

    Add reviews settings access ability

--- app/overrides/add_reviews_to_admin_configuration_sidebar.rb
+++ app/overrides/add_reviews_to_admin_configuration_sidebar.rb
@@ -2,5 +2,5 @@ Deface::Override.new(
   virtual_path: 'spree/admin/shared/sub_menu/_configuration',
   name: 'converted_admin_configurations_menu',
   insert_bottom: '[data-hook="admin_configurations_sidebar_menu"]',
-  text: '<%= configurations_sidebar_menu_item Spree.t(:review_settings, scope: :spree_reviews), edit_admin_review_settings_path %>'
+  text: '<%= configurations_sidebar_menu_item(Spree.t(:review_settings, scope: :spree_reviews), edit_admin_review_settings_path) if can?(:manage, Spree::ReviewSetting) %>'
 )
