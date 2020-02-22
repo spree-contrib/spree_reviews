e5bcf728a5e91165a45d2baa8531e8ff58e5b282
Matthew Kennedy <m.kennedy@me.com>
2020-02-22 22:39:00 +0000

    Slim down

--- app/assets/javascripts/spree/backend/spree_reviews.js
+++
@@ -1,7 +0,0 @@
-//= require jquery.rating
-//= require spree/backend
-
-// Navigating to a page with ratings via TurboLinks shows the radio buttons
-$(document).on('page:load', function () {
-  $('input[type=radio].star').rating();
-});
