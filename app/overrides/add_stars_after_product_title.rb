408dcb14c027ab4221de20c17c589b4c268e0b4f
Matthew Kennedy <m.kennedy@me.com>
2020-02-22 17:20:22 +0000

    improve

--- app/overrides/add_stars_after_product_title.rb
+++ app/overrides/add_stars_after_product_title.rb
@@ -1,6 +1,6 @@
 Deface::Override.new(
   virtual_path: 'spree/products/show',
-  name: 'what',
+  name: 'add_stars_to_product_title',
   insert_after: '.product-details-title',
   partial: 'spree/shared/trigger'
 )
