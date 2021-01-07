8ecb185fdd8760a0f7aa581f4fd97f6cdf4e7270
Matthew Kennedy <m.kennedy@me.com>
2021-01-07 09:56:55 +0000

    Fix permissions.

--- lib/spree_reviews/engine.rb
+++ lib/spree_reviews/engine.rb
@@ -17,6 +17,7 @@ module SpreeReviews
       Dir.glob(File.join(File.dirname(__FILE__), '../../app/**/*_decorator*.rb')) do |c|
         Rails.configuration.cache_classes ? require(c) : load(c)
       end
+      Spree::Ability.register_ability(Spree::ReviewsAbility)
     end
 
     config.to_prepare(&method(:activate).to_proc)
