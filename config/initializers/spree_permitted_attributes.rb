0bbcaa96f2ac8abd6adf4ae87ab3761e36a94996
edmondchui <edmondchui@users.noreply.github.com>
2023-09-01 16:46:45 -0700

    Resolve issue#201
    Use Array#push instead of Array#+ to avoid redefinition of constant.

--- config/initializers/spree_permitted_attributes.rb
+++ config/initializers/spree_permitted_attributes.rb
@@ -1,7 +1,9 @@
 module Spree
   module PermittedAttributes
-    ATTRIBUTES += %i[review_attributes feedback_review_attributes]
-    mattr_reader *ATTRIBUTES
+    _ATTRIBUTES = %i[review_attributes feedback_review_attributes]
+
+    Spree::PermittedAttributes::ATTRIBUTES.push(*_ATTRIBUTES)
+    mattr_reader *_ATTRIBUTES
 
     @@product_attributes += %i[avg_rating reviews_count]
     @@review_attributes = [:rating, :title, :review, :name, :show_identifier]
